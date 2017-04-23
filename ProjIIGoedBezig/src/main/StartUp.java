708a5a6e6e9034c1f902be1fbce5a6bccc8413cf
kenne <kenne@DESKTOP-VG61S2C>
2017-04-23 12:13:50 +0200

    startUpClass, pushTest

---
+++ ProjIIGoedBezig/src/main/StartUp.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import gui.LoginLectorOrganisatieController;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author kenne
+ */
+public class StartUp extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        LoginLectorOrganisatieController root = new LoginLectorOrganisatieController();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
