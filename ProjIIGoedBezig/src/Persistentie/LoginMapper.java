d83139c281dee336e846f0f28d97b40179ce2f6a
kenne <kenne@DESKTOP-VG61S2C>
2017-04-26 18:55:13 +0200

    querydinges opgelost

--- ProjIIGoedBezig/src/Persistentie/LoginMapper.java
+++ ProjIIGoedBezig/src/Persistentie/LoginMapper.java
@@ -17,7 +17,7 @@ import javax.persistence.Query;
 public class LoginMapper {
 
     private final EntityManager em;
-
+    private int index;
     public LoginMapper() {
         em = SQLConnection.getManager();
     }
@@ -25,22 +25,18 @@ public class LoginMapper {
     public boolean checkLogin(String email, String password) throws Exception {
         ContactPersoon CPersoon;
         try {
-            Query query = em.createQuery("SELECT c from ContactPersoon c WHERE c.EmailContactPersoon = :email", ContactPersoon.class).setParameter("email", email);
-            query.setFirstResult(0);
-            query.setMaxResults(1);
-            CPersoon = (ContactPersoon) query.getSingleResult();
-
-//                   new Lector(
-//                    em.createQuery("SELECT c from ContactPersoon c WHERE c.EmailContactPersoon = :email",
-//                            ContactPersoon.class).setParameter("email", email).getSingleResult().getId());
+               // index =(int) em.createQuery("SELECT c.ContactPersoonId FROM ContactPersoon c WHERE c.EmailContactPersoon = :email").setParameter("email", email).getSingleResult();
+           
+                    index= em.createQuery("SELECT c from ContactPersoon c WHERE c.EmailContactPersoon = :email",
+                           ContactPersoon.class).setParameter("email", email).getSingleResult().getId();
+           CPersoon = em.find(ContactPersoon.class, index);
         } catch (Exception ex) {
             return false; //gooit nullpointerexception for some reason
 
         }
-        int UserId = CPersoon.getId();
-
-        LoginUser user = em.createQuery("SELECT u FROM LoginUser u WHERE u.UserId = :id",
-                LoginUser.class).setParameter("id", CPersoon.getId()).getSingleResult();
+        LoginUser user = em.find(LoginUser.class,CPersoon.getId());
+//        LoginUser user = em.createQuery("SELECT u FROM LoginUser u WHERE u.UserId = :id",
+//                LoginUser.class).setParameter("id", index).getSingleResult();
         return user.getPassword().equals(password);//true or false
 
     }
@@ -51,9 +47,9 @@ public class LoginMapper {
     }
 
     public Lector getLectorAdhvUser(LoginUser user) {
-       //
-       Lector l = (Lector) em.createQuery("SELECT c from ContactPersoon c WHERE c.ContactPersoonId = :userId",ContactPersoon.class).setParameter("userId",user.getUserId()).getSingleResult();
-       return l;
+        //
+        Lector l = (Lector) em.createQuery("SELECT c from ContactPersoon c WHERE c.ContactPersoonId = :userId", ContactPersoon.class).setParameter("userId", user.getUserId()).getSingleResult();
+        return l;
     }
 
 }
